f2a891bc2315949123cc4430c47fd6a6f91e28d9
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-07-09 12:23:20 +0530

    Add sample error code

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+function greet(name: string) {
+  console.log("Hello, " + name)
+}
+ 
+function add(a: number, b: number): number {
+  return a + b
+}
+ 
+function divide(a: number, b: number): number {
+  if (b = 0) {
+    throw "Division by zero"
+  }
+  return a / b
+}
+ 
+let result = add(5, "10")
+ 
+greet("Alice")
