796a5e0d53ab58dabdb9e09cfbb35eb6aaa95125
mayank-gupta-03 <gupta.mayank2003@gmail.com>
2025-07-09 15:28:35 +0530

    fix format

--- .ai/io/mayankgupta/github_reviewer.py
+++ .ai/io/mayankgupta/github_reviewer.py
@@ -1,5 +1,5 @@
 import os
-from git import Git 
+from git import Git
 from pathlib import Path
 from ai.chat_gpt import ChatGPT
 from ai.ai_bot import AiBot
@@ -8,8 +8,11 @@ from env_vars import EnvVars
 from repository.github import GitHub
 from repository.repository import RepositoryError
 
-separator = "\n\n----------------------------------------------------------------------\n\n"
-log_file = open('output.txt', 'a')
+separator = (
+    "\n\n----------------------------------------------------------------------\n\n"
+)
+log_file = open("output.txt", "a")
+
 
 def main():
     vars = EnvVars()
@@ -19,41 +22,54 @@ def main():
     github = GitHub(vars.token, vars.owner, vars.repo, vars.pull_number)
 
     remote_name = Git.get_remote_name()
-    
+
     Log.print_green("Remote is", remote_name)
-    changed_files = Git.get_diff_files(remote_name=remote_name, head_ref=vars.head_ref, base_ref=vars.base_ref)
+    changed_files = Git.get_diff_files(
+        remote_name=remote_name, head_ref=vars.head_ref, base_ref=vars.base_ref
+    )
     Log.print_green("Found changes in files", changed_files)
-    if len(changed_files) == 0: 
+    if len(changed_files) == 0:
         Log.print_red("No changes between branch")
 
     for file in changed_files:
         Log.print_green("Checking file", file)
 
         _, file_extension = os.path.splitext(file)
-        file_extension = file_extension.lstrip('.')
+        file_extension = file_extension.lstrip(".")
         if file_extension not in vars.target_extensions:
-            Log.print_yellow(f"Skipping, unsuported extension {file_extension} file {file}")
+            Log.print_yellow(
+                f"Skipping, unsuported extension {file_extension} file {file}"
+            )
             continue
 
         try:
-            with open(file, 'r') as file_opened:
+            with open(file, "r") as file_opened:
                 file_content = file_opened.read()
         except FileNotFoundError:
             Log.print_yellow("File was removed. Continue.", file)
             continue
 
-        if len( file_content ) == 0: 
+        if len(file_content) == 0:
             Log.print_red("File is empty")
             continue
 
-        file_diffs = Git.get_diff_in_file(remote_name=remote_name, head_ref=vars.head_ref, base_ref=vars.base_ref, file_path=file)
-        if len( file_diffs ) == 0: 
+        file_diffs = Git.get_diff_in_file(
+            remote_name=remote_name,
+            head_ref=vars.head_ref,
+            base_ref=vars.base_ref,
+            file_path=file,
+        )
+        if len(file_diffs) == 0:
             Log.print_red("Diffs are empty")
-        
-        Log.print_green(f"Asking AI. Content Len:{len(file_content)} Diff Len: {len(file_diffs)}")
+
+        Log.print_green(
+            f"Asking AI. Content Len:{len(file_content)} Diff Len: {len(file_diffs)}"
+        )
         response = ai.ai_request_diffs(code=file_content, diffs=file_diffs)
 
-        log_file.write(f"{separator}{file_content}{separator}{file_diffs}{separator}{response}{separator}")
+        log_file.write(
+            f"{separator}{file_content}{separator}{file_diffs}{separator}{response}{separator}"
+        )
 
         if AiBot.is_no_issues_text(response):
             Log.print_green("File looks good. Continue", file)
@@ -65,19 +81,28 @@ def main():
             result = False
             for response in responses:
                 if response.line:
-                    result = post_line_comment(github=github, file=file, text=response.text, line=response.line, commit_sha=vars.head_commit_sha)
+                    result = post_line_comment(
+                        github=github,
+                        file=file,
+                        text=response.text,
+                        line=response.line,
+                        commit_sha=vars.head_commit_sha,
+                    )
                 if not result:
-                    result = post_general_comment(github=github, file=file, text=response.text)
+                    result = post_general_comment(
+                        github=github, file=file, text=response.text
+                    )
                 if not result:
                     raise RepositoryError("Failed to post any comments.")
-                    
-def post_line_comment(github: GitHub, file: str, text:str, line: int, commit_sha: str):
+
+
+def post_line_comment(github: GitHub, file: str, text: str, line: int, commit_sha: str):
     Log.print_green("Posting line", file, line, text)
     try:
         git_response = github.post_comment_to_line(
-            text=text, 
-            commit_id=commit_sha, 
-            file_path=file, 
+            text=text,
+            commit_id=commit_sha,
+            file_path=file,
             line=line,
         )
         Log.print_yellow("Posted", git_response)
@@ -86,7 +111,8 @@ def post_line_comment(github: GitHub, file: str, text:str, line: int, commit_sha
         Log.print_red("Failed line comment", e)
         return False
 
-def post_general_comment(github: GitHub, file: str, text:str) -> bool:
+
+def post_general_comment(github: GitHub, file: str, text: str) -> bool:
     Log.print_green("Posting general", file, text)
     try:
         message = f"{file}\n{text}"
@@ -96,6 +122,7 @@ def post_general_comment(github: GitHub, file: str, text:str) -> bool:
     except RepositoryError:
         Log.print_red("Failed general comment")
         return False
+
 
 if __name__ == "__main__":
     main()
